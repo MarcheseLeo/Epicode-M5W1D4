26c5cf7543698a6654973aebff83d11b4896490d
MarcheseLeo <leonardo.lol.ldp@gmail.com>
2026-04-24 17:59:15 +0200

    Some fixes

--- src/components/myMain/MyMain.jsx
+++ src/components/myMain/MyMain.jsx
@@ -44,7 +44,7 @@ export const MyMain = () => {
     }
     const handleLengthChange = (e) => {
         let value = parseInt(e.target.value);
-        
+
         if (isNaN(value)) {
             setLength('');
             return;
