63ccce7cf54145451a30da78f528cfe90e3bedac
MarcheseLeo <leonardo.lol.ldp@gmail.com>
2026-05-05 21:26:52 +0200

    saving theme in localStorage

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,13 +1,20 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const ThemeContext = createContext()
 
 export const ThemeProvider = ({children}) =>{
-    const [theme, setTheme] = useState(true)
+    const [theme, setTheme] = useState(()=>{
+        const savedTheme = localStorage.getItem('app-theme')
+        if(savedTheme !==null)
+            return JSON.parse(savedTheme)
+        return true
+    })
     const computedTheme = theme ? 'light' : 'dark' 
-
     const toggleTheme = () => setTheme(!theme)
 
+    useEffect(()=>{
+        localStorage.setItem('app-theme', JSON.stringify(theme))
+    }, [theme])
     return (
         <ThemeContext.Provider
             value={{
